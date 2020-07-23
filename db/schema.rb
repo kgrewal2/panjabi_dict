44d30838c0d54c00252e67a2ea1922312cc74cb0
Karandeep <kgrewal2@asu.edu>
2020-07-23 16:58:54 -0700

    :card_file_box:  Adding languages to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_20_033837) do
+ActiveRecord::Schema.define(version: 2020_07_23_233821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "languages", force: :cascade do |t|
+    t.string "langcode"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "panjabi_words", force: :cascade do |t|
     t.string "gurmukhi"
