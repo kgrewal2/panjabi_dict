72916acc2f5db80b8c0cd80dc47880914715dd07
Karandeep <kgrewal2@asu.edu>
2020-08-05 22:10:25 -0700

    :card_file_box: :fire: Changing column name in translations table

--- db/migrate/20200806050800_add_language_to_translations.rb
+++ db/migrate/20200806050800_add_language_to_translations.rb
@@ -1,5 +1,5 @@
 class AddLanguageToTranslations < ActiveRecord::Migration[6.0]
   def change
-    add_column :translations, :language, :integer
+    add_column :translations, :language_id, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_06_041429) do
+ActiveRecord::Schema.define(version: 2020_08_06_050800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 2020_08_06_041429) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "panjabi_word_id"
-    t.integer "language"
+    t.integer "language_id"
   end
 
   create_table "users", force: :cascade do |t|
